f3ab38dcebff2c41168fd16ca70be50ce019f437
mahmud0202 <bakytbekovich055@gmail.com>
2024-11-21 08:36:27 +0600

    new

---
+++ components/TaskList.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import TaskItem from "./TaskItem";
+
+interface Task {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+interface TaskListProps {
+  tasks: Task[];
+  toggleTask: (id: number) => void;
+  deleteTask: (id: number) => void;
+  handleEditClickTask: (id: number) => void;
+}
+
+export default function TaskList({
+  tasks,
+  handleEditClickTask,
+  toggleTask,
+  deleteTask,
+}: TaskListProps) {
+  return (
+    <ul className="space-y-2">
+      {tasks.map((task) => (
+        <TaskItem
+          key={task.id}
+          task={task}
+          toggleTask={toggleTask}
+          deleteTask={deleteTask}
+        />
+      ))}
+    </ul>
+  );
+}
